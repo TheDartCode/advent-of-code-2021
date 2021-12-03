c6af347ac0f0e28c8a7c3069e1c2eaba2f84f78f
TheDartCode <giorgos@thedartcode.com>
2021-12-03 12:16:54 +0200

    Bootstrap Day3

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 import day1 from "./day1";
 import day2 from "./day2";
+import day3 from "./day3";
 import { DayFunc, DayPart } from "./types";
 import { PerformanceMonitor } from "./shared/helpers";
 
@@ -13,6 +14,7 @@ type DaysDictionary = {
 const days: DaysDictionary = {
   "1": day1,
   "2": day2,
+  "3": day3,
 };
 
 const day = process.argv[2];
---
+++ src/day3/index.test.ts
@@ -0,0 +1,16 @@
+import day3 from "./index";
+
+const TEST_DATA = ``;
+
+describe("day3", () => {
+  describe("first part", () => {
+    it.skip("satisfies test data", () => {
+      expect(day3("a", TEST_DATA)).toBe("2");
+    });
+  });
+  describe("second part", () => {
+    it.skip("satisfies test data", () => {
+      expect(day3("b", TEST_DATA)).toBe("1");
+    });
+  });
+});
